aa48f0b173feafd1056d4d6bf92cae29ec1a603b
Jeremy Kahn <jeremyckahn@gmail.com>
2010-09-19 22:43:36 -0500

    Added a basic window.

--- src/jui/js/juiCore.js
+++ src/jui/js/juiCore.js
@@ -1,5 +1,7 @@
 juiElement = {
 	init: function(params){
+		this.juiId = 'juiId' + (window.juiId ? window.juiId++ : window.juiId = 0);
+		
 		this.params = params || {};
 	
 		if(!this.params.container){
@@ -11,6 +13,17 @@ juiElement = {
 			this.container = this.params.container; 
 		}
 		
+		this.container.css({
+			position : 'absolute',
+			top : 0,
+			left : 0
+		})
+		.attr('id', this.juiId);
+		
 		return this;
+	},
+	
+	generate: function(){
+		return this.container;
 	}
 };
--- src/jui/js/juiWindow.js
+++ src/jui/js/juiWindow.js
@@ -1,7 +1,45 @@
 function juiWindow(params){
 	
+	var defaults = {
+			windowWidth : 300,
+			windowHandleHeight : 35,
+			windowPaneHeight : 300,
+		},
+		juiId;
+	
 	this.init(params);
-
+	
+	juiId = this.juiId;
+	
+	this.windowHandle = $('<div>', { 
+			class : 'window handle'
+		})
+		.css({
+			position : 'absolute',
+			top : 0,
+			left : 0,
+			height : this.params.handleHeight || defaults.windowHandleHeight,
+			width : this.params.width || defaults.windowWidth
+		});
+		
+	this.windowPane = $('<div>', { 
+			class : 'window pane'
+		})
+		.css({
+			position : 'absolute',
+			top : this.windowHandle.css('height'),
+			left : 0,
+			height : this.params.height || defaults.windowPaneHeight,
+			width : this.params.width || defaults.windowWidth,
+			opacity: .8
+		});
+		
+	this.container.draggable({
+			handle: this.windowHandle
+		})
+		.append(this.windowHandle)
+		.append(this.windowPane);
+	
 	return this;
 }
 
